105774f6f650794d37693862907968440ae2fe71
Unknown <8700106+devildant@users.noreply.github.com>
2019-01-16 20:12:14 +0100

    test

--- octoprint_test/__init__.py
+++ octoprint_test/__init__.py
@@ -25,7 +25,7 @@ class TestPlugin(octoprint.plugin.SettingsPlugin,
 	def sdcard(self, printer, filename, absolutePath, sd_upload_started, success_hook_sdcopy, error_hook_sdcopy, *args, **kwargs):
 		remoteName = "test.gco"
 		sd_upload_started(filename, remoteName)
-		self._logger.info("Starting dummy SDCard upload from {} to {}".format(filename, remote_name))
+		self._logger.info("Starting dummy SDCard upload from {} to {}".format(filename, remoteName))
 		def process():
 			time.sleep(10)
 			self._logger.info("call success method")
