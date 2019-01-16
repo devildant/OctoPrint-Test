9cd6c4ef63ad729d037742db962971da6e097473
Unknown <8700106+devildant@users.noreply.github.com>
2019-01-16 20:08:03 +0100

    test

--- octoprint_test/__init__.py
+++ octoprint_test/__init__.py
@@ -12,7 +12,6 @@ from flask import make_response
 from octoprint.server import user_permission
 from octoprint.events import eventManager, Events
 import threading
-import logging
 
 
 class TestPlugin(octoprint.plugin.SettingsPlugin,
@@ -25,12 +24,11 @@ class TestPlugin(octoprint.plugin.SettingsPlugin,
 	
 	def sdcard(self, printer, filename, absolutePath, sd_upload_started, success_hook_sdcopy, error_hook_sdcopy, *args, **kwargs):
 		remoteName = "test.gco"
-		logger = logging.getLogger(__name__)
 		sd_upload_started(filename, remoteName)
-		logger.info("Starting dummy SDCard upload from {} to {}".format(filename, remote_name))
+		self._logger.info("Starting dummy SDCard upload from {} to {}".format(filename, remote_name))
 		def process():
 			time.sleep(10)
-			logger.info("call success method")
+			self._logger.info("call success method")
 			success_hook_sdcopy(filename, remoteName, 10)
 			
 		thread = threading.Thread(target=process)
